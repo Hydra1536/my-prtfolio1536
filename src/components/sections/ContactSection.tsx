c34baac9d0c82695d41fb192942c9e5b72f0a652
MD Rezaul Karim <152043654+Hydra1536@users.noreply.github.com>
2026-01-13 13:06:54 +0600

    Update ContactSection.tsx

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -246,7 +246,7 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         className="mt-24 text-center text-muted-foreground"
       >
         <p className="text-sm">
-          © {new Date().getFullYear()} I aim to build secure, scalable systems that power real-world applications.⭐
+          © {new Date().getFullYear()}© 2026 Md Rezaul Karim. I aim to build secure, scalable systems that power real-world applications.⭐
         </p>
       </motion.div>
     </section>
